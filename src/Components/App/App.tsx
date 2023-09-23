0cdd8da266d080605973c4bbd6261a6110d1b3c8
River <riverliway@gmail.com>
2023-09-23 23:56:37 +0300

    feat: add types

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+/**
+ * The entire application
+ */
 const App: React.FC = () => {
   return (
     <div>
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,35 @@
 
-export interface Coordinate {
+export enum Player {
+  WHITE = 'white',
+  BLACK = 'black'
+}
+
+export enum Piece {
+  PAWN = 'pawn',
+  ROOK = 'rook',
+  KNIGHT = 'knight',
+  BISHOP = 'bishop',
+  QUEEN = 'queen',
+  KING = 'king'
+}
+
+export interface Position {
   x: number
   y: number
+}
+
+export interface Move {
+  player: Player
+  piece: Piece
+  from: Position
+  to: Position
+  getNotation: () => string
+}
+
+export type Board = ({ player: Player, piece: Piece } | undefined)[][]
+
+export interface Game {
+  board: Board
+  moves: Move[]
+  winner?: Player
 }
